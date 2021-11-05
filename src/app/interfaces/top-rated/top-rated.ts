5000063ca346e6088f73f9533af995c179773837
Kaustubh Solanki <Kaustubh@technokart.in>
2021-11-05 12:52:59 +0530

    Interface for top_rated : pending

--- src/app/interfaces/top-rated/top-rated.ts
+++ src/app/interfaces/top-rated/top-rated.ts
@@ -1,7 +1,14 @@
 export interface TopRated {
     page: number,
     results : {
-        poster_path : string,
+        poster_path : string | null,
+        adult: boolean,
+        overview: string,
+        release_date: string,
+        genre_ids: number[],
+        id: number,
+        original_tital: string,
+        original_language: string,
         
     }
 }
